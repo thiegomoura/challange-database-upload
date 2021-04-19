66cb471e26ad2240cc98386b07190a876b63dbf3
thiegomoura <thi-ego@hotmail.com>
2021-04-18 23:06:23 -0400

    init challenge

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
+import { getCustomRepository, getRepository } from 'typeorm';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
@@ -8,11 +10,26 @@ import { Router } from 'express';
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionRepository = getCustomRepository(TransactionsRepository);
+
+  const repositories = await transactionRepository.find();
+
+  return response.json(repositories);
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category } = request.body;
+
+  const createTransaction = new CreateTransactionService();
+
+  const transaction = await createTransaction.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,50 @@
 // import AppError from '../errors/AppError';
 
+import { getCustomRepository, getRepository } from 'typeorm';
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: string;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    const findCategory = await categoryRepository.findOne({
+      where: { category },
+    });
+
+    let categoryId = findCategory?.id;
+
+    if (!findCategory) {
+      const newCategory = categoryRepository.create({
+        title: category,
+      })
+      await categoryRepository.save(newCategory);
+      categoryId = newCategory.id;
+    }
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: categoryId,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
